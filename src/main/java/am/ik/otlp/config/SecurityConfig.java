2db2f09148f2b1d5d7d017a04473ccdf13ac5994
Toshiaki Maki <makingx@gmail.com>
2025-05-28 14:55:59 +0900

    Add SecurityConfig

---
+++ src/main/java/am/ik/otlp/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package am.ik.otlp.config;
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration(proxyBeanMethods = false)
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		return http
+			.authorizeHttpRequests((authorize) -> authorize.requestMatchers(EndpointRequest.toAnyEndpoint())
+				.permitAll()
+				.anyRequest()
+				.authenticated())
+			.csrf(AbstractHttpConfigurer::disable)
+			.build();
+	}
+
+}
